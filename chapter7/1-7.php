1def46d6dfe83896161b93211361fb34a4909cca
vari0710 <himazinv0710@gmail.com>
2025-04-01 23:39:59 +0900

    phpの基礎

--- chapter7/1-7.php
+++ chapter7/1-7.php
@@ -7,15 +7,31 @@
 </head>
 <body>
     <?php
-        $hashira = [
-            '水柱' => '富岡義勇'
-        ];
-        echo $hashira['水柱'];
-        $hashira["水柱"] = "鱗滝左近寺";
+        $a = ["a", "i", "u", "e", "o"];
 
-        echo "<br/>";
-        
-        echo $hashira["水柱"];
+        //最後に追加される
+        array_push($a, "あ");
+
+        // 一つづつしか追加されない,処理が早い
+        $a[] = "い";
+        var_dump($a);
+
+        //配列に配列をくっつけることができる,非破壊
+        $b = ["ka","ki"];
+        $merged = array_merge($a, $b);
+        var_dump($merged);
+
+        //配列の先頭に値を持ってくる
+        array_unshift($a,"ka");
+        var_dump($a);
+
+        //先頭からとって消す
+        $first = array_shift($a);
+        var_dump($first);
+
+        //末尾の値を返す
+        $first = array_pop($a);
+        var_dump($first);
     ?>
 </body>
 </html>
